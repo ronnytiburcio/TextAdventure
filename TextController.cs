4e11533f3c16f7698ae7652b06ab983692ddae6c
ronnytiburcio <37945503+ronnytiburcio@users.noreply.github.com>
2018-05-10 21:59:42 -0400

    Updated Story

--- TextController.cs
+++ TextController.cs
@@ -6,7 +6,7 @@ public class TextController : MonoBehaviour {
 	
 	public Text text;
 	
-	private enum States {Bed, Mirror, Door, Clothes, WriteOnDesk, ReadBackwards, WritingOnStomach, ReadWritingOnStomach, GrabKey, Freedom};
+	private enum States {Bed, Mirror, DownStairs, UpStairs, KeepLooking, LookAtPictures, Ignoreit, Door, SearchCloset, InvestigateSafe, RunToBathroom, LookForExit, Clothes, WriteOnDesk, ReadBackwards, WritingOnStomach, ReadWritingOnStomach, GrabKey, Freedom};
 	private States MyState;  
 	
 	
@@ -34,6 +34,24 @@ public class TextController : MonoBehaviour {
 		state_WriteOnDesk();
 		}else if (MyState==States.GrabKey){
 		state_GrabKey();
+		}else if (MyState==States.LookForExit){
+		state_LookForExit();
+		}else if (MyState==States.RunToBathroom){
+			state_RunToBathroom();
+		}else if (MyState==States.SearchCloset){
+			state_SearchCloset();
+		}else if (MyState==States.InvestigateSafe){
+		state_InvestigateSafe();
+		}else if (MyState==States.LookAtPictures){
+		state_LookAtPictures();
+		}else if (MyState==States.Ignoreit){
+		state_Ignoreit();
+		}else if (MyState==States.UpStairs){
+		state_UpStairs();
+		}else if (MyState==States.KeepLooking){
+		state_KeepLooking();
+		}else if (MyState==States.DownStairs){
+		state_DownStairs();
 		}
 		
 	}
@@ -117,10 +135,104 @@ public class TextController : MonoBehaviour {
 	text.text="You slowly walked over to her side of the bed trying to ignore the squeeky tiles"+
 			  " on the ground. You slowly reach your arm under the pillow and you feel a cold piece"+
 			  " of shape metal. Its the Key. Out of excitement you agressiveley pull your arm out"+
-			  " quickly open the door. You made it you son of a gun. You are free. naked but free.\n\n"+
-			  "Press Spacebar to start all over, other wise you can close this app.";
-		if (Input.GetKeyDown(KeyCode.Space)){
-		MyState=States.Bed;
+			  " quickly open the door. You made it but it seems like you are in some kind of institution."+
+			  " You hear some noise, there is someone coming. What do I do?\n\n"+
+			  "Press L to look for exit, Press C to go to closet, Press B to run in bathroom.";
+		if (Input.GetKeyDown(KeyCode.L)){
+		MyState=States.LookForExit;
+		}
+		if (Input.GetKeyDown(KeyCode.B)){
+			MyState=States.RunToBathroom;
+		}
+		if (Input.GetKeyDown(KeyCode.C)){
+			MyState=States.SearchCloset;
+		}
+	}
+	void state_LookForExit(){
+		text.text="While you are roaming and looking for an exit, you realize that you have no clothes on."+
+				  " You have to find a way to blend in before they find out you dont belong here.\n\n"+
+				  "Press R to return";
+		if (Input.GetKeyDown(KeyCode.R)){
+			MyState=States.GrabKey;
+		}
+	}
+	void state_RunToBathroom(){
+		text.text="You see a door slightly open and it looks like a bathroom. You begin to see if there are"+
+				  " any clothes laying around and no lucky. Someone walks in so you quickly tip toe to the"+
+				  " shower and hide there for about a few minutes until the guard finishes. Bathroom was a bad idea.\n\n"+
+				  "Press R to return";
+		if (Input.GetKeyDown(KeyCode.R)){
+			MyState=States.GrabKey;
+		}
+	}
+	void state_SearchCloset(){
+		text.text=" You run inside the closet before anyone can see. Finally, some clothing. I put on"+
+				  " some street clothes and I find a guards ID so I put in on along with a hat to cover my face."+
+				  " There seems to be other interesting things in the closet. What is this place anyway. \n\n"+
+				  "Press I to ignore it, S to investigate the Safe, L to look at the pictures on the wall";
+		if (Input.GetKeyDown(KeyCode.S)){
+			MyState=States.InvestigateSafe;
+		}
+		if (Input.GetKeyDown(KeyCode.L)){
+			MyState=States.LookAtPictures;
+		}
+		if (Input.GetKeyDown(KeyCode.I)){
+			MyState=States.Ignoreit;
+		}
+	}
+	void state_InvestigateSafe(){
+		text.text="I wonder how you open this. Maybe it's already open. Lets see......."+
+				  " Whoa. I guess they bring people over here to use them as lab rats."+
+				  " Why did I waste my time going through this, I gotta go. \n\n"+
+				  "Press R to return";
+		if (Input.GetKeyDown(KeyCode.R)){
+			MyState=States.SearchCloset;
+		}
+	}
+	void state_LookAtPictures(){
+		text.text="There are some really creepy pictures on the wall. It's just a bunch of dead"+
+				  " bodies. O god, I feel like puking. \n\n"+
+				  "Press R to Return";
+		if (Input.GetKeyDown(KeyCode.R)){
+			MyState=States.SearchCloset;
+		}
+	}
+	void state_Ignoreit(){
+		text.text="You decide to ignore everything and just get out. You walk down the hall and see two"+
+				  " other on duty guards and you nod your head and they do the same. That was close."+
+				  " You notice there is a set of stairs at the end of the hall. \n\n"+
+				  "Press U to go upstairs, D to go downstairs, L to keep looking for exit on current floor.";
+		if (Input.GetKeyDown(KeyCode.U)){
+			MyState=States.UpStairs;
+		}
+		if (Input.GetKeyDown(KeyCode.L)){
+			MyState=States.KeepLooking;
+		}
+		if (Input.GetKeyDown(KeyCode.D)){
+			MyState=States.DownStairs;
+		}
+	}
+	void state_UpStairs(){
+		text.text="You walk up stairs and all you see is a bunch of doctors with bloody clothes walking"+
+				  " from room to room. \n\n"+
+				  "Press R to Return";
+		if (Input.GetKeyDown(KeyCode.R)){
+			MyState=States.Ignoreit;
+		}
+	}
+	void state_KeepLooking(){
+		text.text=" You decide to stay on the same floor to see if you find an exit and no luck. \n\n"+
+				  "Press R to Return";
+		if (Input.GetKeyDown(KeyCode.R)){
+			MyState=States.Ignoreit;
+		}
+	}
+	void state_DownStairs(){
+		text.text="You walk downstairs and you see a red door. You pray and hope this leads to streets."+
+				  " You are free you son of a gun. Now go home and never say a word about this. \n\n"+
+				  "Press R to play again";
+		if (Input.GetKeyDown(KeyCode.R)){
+			MyState=States.Bed;
 		}
 	}
 }
